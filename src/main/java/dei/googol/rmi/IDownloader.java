b3bb25649528e1a96f3061916f0378013b1697d9
Miguel Ângelo Pinto da Cunha <mpintodacunha@gmail.com>
2026-03-30 20:12:50 +0100

    refactor(rmi): remove startDownloading and add getName to IDownloader

--- src/main/java/dei/googol/rmi/IDownloader.java
+++ src/main/java/dei/googol/rmi/IDownloader.java
@@ -3,6 +3,20 @@ package dei.googol.rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Remote interface exposed by each Downloader instance.
+ *
+ * <p>Downloaders register themselves in the RMI Registry so that the Gateway
+ * can query how many are active. The actual crawl loop runs on a background
+ * thread inside each Downloader and does not need to be invoked remotely.
+ */
 public interface IDownloader extends Remote {
-    void startDownloading() throws RemoteException;
+
+    /**
+     * Returns the unique name of this downloader (e.g., {@code "Downloader1"}).
+     *
+     * @return downloader name
+     * @throws RemoteException on RMI communication failure
+     */
+    String getName() throws RemoteException;
 }
